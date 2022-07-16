c4238e5b91c8a00cb516f851cc4c3b48810976d6
alhamdhany1310 <dhani.lankmania@gmail.com>
2022-07-16 11:37:01 +0800

    task expressjs

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,10 +1,21 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
+  // database: 'sequelize-task',
+  // host: 'localhost',
+  // username: 'root',
+  // password: '',
+  // dialect: 'mysql',
   database: 'dcpqo6ljgcmudv',
   host: 'ec2-54-152-28-9.compute-1.amazonaws.com',
   username: 'zlyjtchmmedelo',
   password: '392e05eb10d2fd5a67466d517597ac2817ad0555bdc0170d4df81c03297818f5',
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false, // This line will fix new error
+    },
+  },
 });
 
 (async () => {
